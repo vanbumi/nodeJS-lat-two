c6bb7f6aaa7d312fb0ecf834d626199b275385b1
vanbumi <jsp.dio@gmail.com>
2016-02-14 22:57:17 +0700

    first commit

---
+++ module1.js
@@ -0,0 +1,9 @@
+//How module works
+
+var m2 = require('./module2');
+//console.log(m2); instead change to m2(); callback
+
+m2();
+
+
+
---
+++ module2.js
@@ -0,0 +1,15 @@
+var a = 1;
+
+//module.exports.a = a;
+//module.exports.b = 2;
+
+//you can do short this
+
+//exports.a = a;
+//exports.b = 2;
+
+//or u can create function
+
+module.exports = function() {
+  console.log('module dua!');
+};
